bd30a97a8580a10d635e2239e23edbd3c6007cd5
Soumen Rakshit <rakshitsoumen525@gmail.com>
2025-08-20 14:27:07 +0530

    Your commit message here

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,8 @@ app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
